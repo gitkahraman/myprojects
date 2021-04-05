79ae0c791ebcc8f6eea073c5af9a9f39b8ab244f
gitkahraman <63319455+gitkahraman@users.noreply.github.com>
2021-04-05 18:11:18 +0300

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,72 @@
+{
+   "type":"FeatureCollection",
+   "features":[
+      {
+         "type":"Feature",
+         "properties":{
+            "stage":"LEAD",
+            "month":"mapMonth(""{\"LEAD\": \"3\",\"AO\": \"\",\"FAO\": \"\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "LEAD"")",
+            "year":"mapYear(""{\"LEAD\": \"2021\",\"AO\": \"\",\"FAO\": \"\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "LEAD"")"
+         },
+         "geometry":{
+            "coordinates":[
+               
+            ],
+            "type":"Point"
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "stage":"LEAD",
+            "month":"mapMonth(""{\"LEAD\": \"3\",\"AO\": \"3\",\"FAO\": \"3\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "LEAD"")",
+            "year":"mapYear(""{\"LEAD\": \"2021\",\"AO\": \"2021\",\"FAO\": \"2021\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "LEAD"")"
+         },
+         "geometry":{
+            "coordinates":[
+               2.1487739,
+               41.3984814
+            ],
+            "type":"Point"
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "stage":"AO",
+            "month":"mapMonth(""{\"LEAD\": \"3\",\"AO\": \"3\",\"FAO\": \"3\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "AO"")",
+            "year":"mapYear(""{\"LEAD\": \"2021\",\"AO\": \"2021\",\"FAO\": \"2021\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "AO"")"
+         },
+         "geometry":{
+            "coordinates":[
+               2.1487739,
+               41.3984814
+            ],
+            "type":"Point"
+         }
+      },
+      {
+         "type":"Feature",
+         "properties":{
+            "stage":"FAO",
+            "month":"mapMonth(""{\"LEAD\": \"3\",\"AO\": \"3\",\"FAO\": \"3\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "FAO"")",
+            "year":"mapYear(""{\"LEAD\": \"2021\",\"AO\": \"2021\",\"FAO\": \"2021\",\"BOUGHT\": \"\",\"SOLD\": \"\"}",
+            "FAO"")"
+         },
+         "geometry":{
+            "coordinates":[
+               2.1487739,
+               41.3984814
+            ],
+            "type":"Point"
+         }
+      }
+   ]
+}
